d0fe73b908c1598eb25eb46265d7246b36d3387e
Ganiyu Basirat Olanike <nikkibrownie217@gmail.com>
2023-04-26 11:05:13 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useEffect } from 'react'
+
+function App() {
+  useEffect(()=>{
+    if ('serviceWorker' in navigator){
+      window.addEventListener('load', ()=>{
+        navigator.serviceWorker.register('/sw.js')
+        .then(registration =>{
+          console.log('SW registered:', registration);
+        })
+        .catch(registrationError =>{
+          console.log('SW registration failed:', registrationError);
+        })
+      })
+    }
+  },[])
+
+
+
+  return (
+    <>
+      
+    </>
+  
+  )
+}
+
+export default App
